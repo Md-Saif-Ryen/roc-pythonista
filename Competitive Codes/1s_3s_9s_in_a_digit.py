be15a8eb9840acdf09ceb78d71cce47053a34b4c
Roc Tanweer <71582699+RocTanweer@users.noreply.github.com>
2020-12-22 20:43:54 +0530

    Added useful comments!

--- Competitive Codes/1s_3s_9s_in_a_digit.py
+++ Competitive Codes/1s_3s_9s_in_a_digit.py
@@ -1,5 +1,8 @@
 class ones_threes_nines:
     """gives number of nines threes and ones that an integet is consist of"""
+#For example----
+#if you enter 20,it will print--
+#nines:2, threes:0, ones:2
 
     def __init__(self, number):
         self.answer = f"nines:{number//9}, threes:{number%9//3}, ones:{number%9%3}"
